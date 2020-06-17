24b6555b4abd491153882df773b9d0adb1891870
KucherenkoIvan <stysha.72@mail.ru>
2020-06-17 19:56:35 +0500

    Рабочее первое задание

--- PL/python/main.py
+++ PL/python/main.py
@@ -1,36 +1,38 @@
-import matplotlib as mpl
+import os
+import pyodbc as database
+
+import numpy as np
 import matplotlib.pyplot as plt
-import math
-import cv2
+
+def graphics(K):
+    x = np.arange(1, 8)
+    y = np.random.randint(1, 20, size = 7)
+
+    fig, ax = plt.subplots()
+
+    ax.bar(x, y)
+
+    ax.set_facecolor('seashell')
+    fig.set_facecolor('floralwhite')
+    fig.set_figwidth(12)    #  ширина Figure
+    fig.set_figheight(6)    #  высота Figure
+
+    plt.show()
+
+def sql(N):
+    connectionString = (r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
+        r"DBQ=" + os.path.realpath('data/database.mdb') + ";")
+    connection = database.connect(connectionString)
+    cursor = connection.cursor()
+    s = "select top " + str(N) + " user.occupation, count(rating.id) from user inner join rating on user.id = rating.userid group by user.occupation order by count(rating.id) desc"    
+    try:
+        cursor.execute(s)
+        for row in cursor.fetchall():
+            print(str(row))
+    except:
+        print('exception!')
 
 
-dpi = 80
-fig = plt.figure(dpi = dpi, figsize = (512 / dpi, 384 / dpi) )
-mpl.rcParams.update({'font.size': 10})
 
-plt.axis([0, 10, -1.5, 1.5])
-
-plt.title('Sine & Cosine')
-plt.xlabel('x')
-plt.ylabel('F(x)')
-
-xs = []
-sin_vals = []
-cos_vals = []
-
-x = 0.0
-while x < 10.0:
-    sin_vals += [ math.sin(x) ]
-    cos_vals += [ math.cos(x) ]
-    xs += [x]
-    x += 0.1
-
-plt.plot(xs, sin_vals, color = 'blue', linestyle = 'solid',
-         label = 'sin(x)')
-plt.plot(xs, cos_vals, color = 'red', linestyle = 'dashed',
-         label = 'cos(x)')
-
-plt.legend(loc = 'upper right')
-plt.show()
-
-#fig.savefig('trigan.png')
+graphics(0)
+#select distinct top 5 user.id from user inner join rating on rating.userid = user.id where user.gender = 'm' and rating.rate = 3 order by user.id desc
